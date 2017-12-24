0545cff46f931ef30ab59eec9f9a22acb02d2a58
Alan K <afksavish@gmail.com>
2017-12-24 19:02:33 +0200

    Second part of Corruption Checksum problem

--- apps/corruption_checksum/lib/corruption_checksum.ex
+++ apps/corruption_checksum/lib/corruption_checksum.ex
@@ -8,14 +8,22 @@ defmodule CorruptionChecksum do
 
   ## Example
 
-      iex> CorruptionChecksum.solve("23    4    5\\n1    2    3\\n5    6    5")
-      22
+      iex> CorruptionChecksum.solve("5    9    2    8\\n9    4    7    3")
+      7
   """
   def solve(input_string) do
     input_string
     |> String.split("\n")
     |> Enum.map(&(&1 |> String.split("    ")))
     |> Enum.map(fn row -> Enum.map(row, &(&1 |> String.to_integer())) end)
-    |> Enum.reduce(0, fn row, acc -> acc + Enum.max(row) - Enum.min(row) end)
+    |> Enum.reduce(0, fn row, acc ->
+      acc +
+        hd(
+          for d1 <- row,
+              d2 <- row,
+              rem(d1, d2) == 0 and d1 != d2,
+              do: div(d1, d2)
+        )
+    end)
   end
 end
